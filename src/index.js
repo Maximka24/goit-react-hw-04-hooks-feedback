0b4ad6696810e47d17432144cb773a878dcb33a2
Kovalenko Makcim <max.kow@yandex.ru>
2021-09-07 17:46:44 +0300

    дз4
    исправление ошибок. Удаление не нужных файлов

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import './index.css';
-// import App from "./Components/AppClass";
-import App from "./Components/AppHook";
+
+import App from "./Components/App";
 
 ReactDOM.render(
   <React.StrictMode>
